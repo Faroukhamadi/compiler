fd3821e7737f5de4c37e5c527bfccee84abafcac
Faroukhamadi <faroukhamadi1@outlook.com>
2022-09-01 09:29:14 +0100

    add AST to my parser

--- Parser.java
+++ Parser.java
@@ -1,8 +1,13 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public final class Parser {
-  // SETUP
 
+  // SETUP
   private static final String EXAMPLE_INPUT = "{\n" +
       "    \"version\": 17,\n" +
       "    \"bundles\": [\n" +
@@ -24,6 +29,7 @@ public final class Parser {
 
   private final String input;
   private int pos = 0;
+  private final Deque<Object> stack = new ArrayDeque<>();
 
   public Parser(String input) {
     this.input = input;
@@ -47,6 +53,7 @@ public final class Parser {
     if (last < 0) {
       return false;
     }
+    stack.push(input.substring(pos + 1, pos + last + 1));
     pos += last + 2;
     skipWhitespace();
     return true;
@@ -58,8 +65,12 @@ public final class Parser {
     }
     Scanner scanner = new Scanner(input.substring(pos));
     String num = scanner.useDelimiter("[^0-9]").next();
-    pos += num.length();
-    return num.length() > 0;
+    if (num.length() > 0) {
+      pos += num.length();
+      stack.push(Integer.parseInt(num));
+      return true;
+    }
+    return false;
   }
 
   private boolean parseChar(char c) {
@@ -91,11 +102,21 @@ public final class Parser {
   // OBJECT ::= "{" (PAIR ("," PAIR)* )? "}"
   private boolean parseObject() {
     int pos0 = pos;
+    int stack0 = stack.size();
     boolean success = parseChar('{') && parsePairs() && parseChar('}');
     if (!success) {
       pos = pos0;
+      return false;
     }
-    return success;
+
+    HashMap<String, Object> object = new HashMap<>();
+    while (stack.size() > stack0) {
+      Object value = stack.pop();
+      String string = (String) stack.pop();
+      object.put(string, value);
+    }
+    stack.push(object);
+    return true;
   }
 
   // (PAIR ("," PAIR)* )?
@@ -131,11 +152,19 @@ public final class Parser {
   // ARRAY ::= "[" (VALUE ("," VALUE)* )? "]"
   private boolean parseArray() {
     int pos0 = pos;
+    int stack0 = stack.size();
     boolean success = parseChar('[') && parseValues() && parseChar(']');
     if (!success) {
       pos = pos0;
+      return false;
     }
-    return success;
+    ArrayList<Object> array = new ArrayList<>();
+    while (stack.size() > stack0) {
+      array.add(stack.pop());
+    }
+    Collections.reverse(array);
+    stack.push(array);
+    return true;
   }
 
   // (VALUE ("," VALUE)* )?
